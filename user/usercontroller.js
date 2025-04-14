9263ef3ad19fc7901a21d02e6f4393abdef702ed
pramod <124747634+pramodtharu12454@users.noreply.github.com>
2025-04-14 21:57:31 +0545

    register addDb

--- user/usercontroller.js
+++ user/usercontroller.js
@@ -1,8 +1,13 @@
 import express from "express";
+import UserTable from "./schema.model.js";
 const router = express.Router();
 
 router.post("/user/register", (req, res) => {
-  return res.status(201).send({ message: "registered...." });
+  async (req, res) => {
+    const newUser = req.body;
+    await UserTable.create(newUser);
+    return res.status(201).send({ message: "registered...." });
+  };
 });
 
 router.post("/user/login", (req, res) => {
